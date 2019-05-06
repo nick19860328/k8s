ca38f8d6be65597db87743bf6f7c7ad6bff4fc39
Charles <charles@Charles-MacBook-Pro.local>
2019-05-06 10:38:35 +0800

    040

--- 040-install-helm.sh
+++ 040-install-helm.sh
@@ -4,7 +4,7 @@ set -xe
 
 sudo snap install helm --classic
 
-helm init --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.8.1
+helm init --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.13.1
 
 curl -fsSL https://raw.githubusercontent.com/fishworks/gofish/master/scripts/install.sh | bash
 gofish init
