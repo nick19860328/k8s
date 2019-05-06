901da77906199301e4589c168dce8405af13bd60
Charles <charles@Charles-MacBook-Pro.local>
2019-05-06 08:38:27 +0800

    020

--- 020-install-kubeadm.sh
+++ 020-install-kubeadm.sh
@@ -3,9 +3,9 @@
 set -xe
 
 apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
