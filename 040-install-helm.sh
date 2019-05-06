d892edb1668e611d70066955b31994fb734210ae
Charles <charles@Charles-MacBook-Pro.local>
2019-05-06 10:58:13 +0800

    040

--- 040-install-helm.sh
+++ 040-install-helm.sh
@@ -4,9 +4,6 @@ set -xe
 
 sudo snap install helm --classic
 
-helm init --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.13.1
+helm init --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.13.1 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
 
-curl -fsSL https://raw.githubusercontent.com/fishworks/gofish/master/scripts/install.sh | bash
-gofish init
-
-gofish install chartmuseum
+helm install stable/chartmuseum
