a9b91d9482201db314cf6ee55396b506b1695efe
Charles <charles@Charles-MacBook-Pro.local>
2019-05-06 10:31:30 +0800

    040

--- 040-install-helm.sh
+++ 040-install-helm.sh
@@ -5,3 +5,5 @@ set -xe
 sudo snap install helm --classic
 
 helm init --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.8.1
+
+gofish install chartmuseum
