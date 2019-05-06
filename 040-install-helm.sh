b895f2162f64500fdff8e8056333357b3f10e921
Charles <charles@Charles-MacBook-Pro.local>
2019-05-06 11:07:39 +0800

    040

--- 040-install-helm.sh
+++ 040-install-helm.sh
@@ -4,6 +4,6 @@ set -xe
 
 sudo snap install helm --classic
 
-helm init --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.13.1 --stable-repo-url=https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
+helm init --upgrade --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.13.1 --stable-repo-url=https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
 
 helm install stable/chartmuseum
