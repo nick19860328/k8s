3b28afb8b59d9a7c80b28323cc2877c62f0f391d
Charles <charles@Charles-MacBook-Pro.local>
2019-05-06 10:15:58 +0800

    040

--- 040-install-helm.sh
+++ 040-install-helm.sh
@@ -4,4 +4,4 @@ set -xe
 
 sudo snap install helm --classic
 
-helm init --history-max 200
+helm init --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.8.1
