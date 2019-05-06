27287f87dd089da8485c531e1ff14dbedaffe211
Charles <charles@Charles-MacBook-Pro.local>
2019-05-06 12:40:02 +0800

    040

--- 040-install-helm.sh
+++ 040-install-helm.sh
@@ -5,8 +5,12 @@ set -xe
 sudo snap install helm --classic
 
 helm init --upgrade --history-max 200 --tiller-image=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.13.1 --stable-repo-url=https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
 
 apt install subversion
 svn export https://github.com/helm/charts/trunk/stable/chartmuseum chartmuseum
 helm package ./chartmuseum
-helm install --name chartmuseum chartmuseum-0.1.0.tgz
+helm install --name chartmuseum chartmuseum-2.3.0.tgz
