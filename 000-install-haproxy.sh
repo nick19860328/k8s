d8b7b2543d878df8a55cdbe5a0f7cf7776499492
Charles <charles@Charles-MacBook-Pro.local>
2019-05-08 02:38:05 +0800

    000

--- 000-install-haproxy.sh
+++ 000-install-haproxy.sh
@@ -2,12 +2,18 @@
 
 set -xe
 
+cat <<EOF >>/etc/hosts
+192.168.31.70 k8s01
+192.168.31.232 k8s02
+192.168.31.231 k8s03
+EOF
+
+apt update
 apt install -y haproxy
 
-mv /etc/haproxy/haproxy.cfg{,.original}
-cat <<EOF >/etc/haproxy/haproxy.cfg
+cat <<EOF >>/etc/haproxy/haproxy.cfg
 frontend k8s-api
-  bind 192.168.0.70:443
+  bind 192.168.31.70:443
   bind 127.0.0.1:443
   mode tcp
   option tcplog
@@ -19,9 +25,9 @@ backend k8s-api
   option tcp-check
   balance roundrobin
   default-server inter 10s downinter 5s rise 2 fall 2 slowstart 60s maxconn 250 maxqueue 256 weight 100
-  server k8s01 192.168.0.70:6443 check
-  server k8s02 192.168.0.232:6443 check
-  server k8s03 192.168.0.231:6443 check
+  server k8s01 192.168.31.70:6443 check
+  server k8s02 192.168.31.232:6443 check
+  server k8s03 192.168.31.231:6443 check
 EOF
 
 systemctl restart haproxy
