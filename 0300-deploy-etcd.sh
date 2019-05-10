4f474c5720b4ceb229bbcc567859523efe68751c
Charles <charles@Charles-MacBook-Pro.local>
2019-05-11 06:51:25 +0800

    0300

---
+++ 0300-deploy-etcd.sh
@@ -0,0 +1,44 @@
+# Update HOST0, HOST1, and HOST2 with the IPs or resolvable names of your hosts
+export HOST0=192.168.31.70
+export HOST1=192.168.31.232
+export HOST2=192.168.31.231
+
+# Create temp directories to store files that will end up on other hosts.
+mkdir -p /tmp/${HOST0}/ /tmp/${HOST1}/ /tmp/${HOST2}/
+
+ETCDHOSTS=(${HOST0} ${HOST1} ${HOST2})
+NAMES=("infra0" "infra1" "infra2")
+
+for i in "${!ETCDHOSTS[@]}"; do
+HOST=${ETCDHOSTS[$i]}
+NAME=${NAMES[$i]}
+cat << EOF > /tmp/${HOST}/kubeadmcfg.yaml
+apiVersion: "kubeadm.k8s.io/v1beta1"
+kind: ClusterConfiguration
+etcd:
+    local:
+        serverCertSANs:
+        - "${HOST}"
+        peerCertSANs:
+        - "${HOST}"
+        extraArgs:
+            initial-cluster: ${NAMES[0]}=https://${ETCDHOSTS[0]}:2380,${NAMES[1]}=https://${ETCDHOSTS[1]}:2380,${NAMES[2]}=https://${ETCDHOSTS[2]}:2380
+            initial-cluster-state: new
+            name: ${NAME}
+            listen-peer-urls: https://${HOST}:2380
+            listen-client-urls: https://${HOST}:2379
+            advertise-client-urls: https://${HOST}:2379
+            initial-advertise-peer-urls: https://${HOST}:2380
+EOF
+done
+
+
+
+
+ USER=vagrant
+ HOST=${HOST1}
+ scp -r /tmp/${HOST}/* ${USER}@${HOST}:
+ ssh ${USER}@${HOST}
+ USER@HOST $ sudo -Es
+ root@HOST $ chown -R root:root pki
+ root@HOST $ mv pki /etc/kubernetes/
--- Vagrantfile
+++ Vagrantfile
@@ -2,19 +2,19 @@ Vagrant.configure("2") do |config|
   config.vm.define "k8s01" do |subconfig|
     subconfig.vm.box = "ubuntu/xenial64"
     subconfig.vm.hostname = "k8s01"
-    subconfig.vm.network :private_network, ip: "192.168.31.70"
+    subconfig.vm.network "private_network", ip: "192.168.31.70"
   end
 
   config.vm.define "k8s02" do |subconfig|
     subconfig.vm.box = "ubuntu/xenial64"
     subconfig.vm.hostname = "k8s02"
-    subconfig.vm.network :private_network, ip: "192.168.31.232"
+    subconfig.vm.network "private_network", ip: "192.168.31.232"
   end
 
   config.vm.define "k8s03" do |subconfig|
     subconfig.vm.box = "ubuntu/xenial64"
     subconfig.vm.hostname = "k8s03"
-    subconfig.vm.network :private_network, ip: "192.168.31.231"
+    subconfig.vm.network "private_network", ip: "192.168.31.231"
   end
   
   config.vm.provision "shell", inline: <<-SHELL
